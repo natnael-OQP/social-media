70296f9ff2ff86cf66a12a5ad9e291f2eea83b65
papa-react <makiya112545@gmail.com>
2021-11-13 20:00:04 -1200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,6 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <h1 className="text-xl text-indigo-500 font-bold" >hello world</h1>
+    <h1 className="text-xl text-indigo-500 font-bold" >hello new</h1>
   )
 }
